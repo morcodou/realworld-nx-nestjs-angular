7de9af65218327b5131a5c7cec3c84bc95425425
Nhan Nguyen <nhanpublic@gmail.com>
2021-03-25 03:27:05 +0700

    update api app

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,13 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get('hello')
-  getData() {
-    return this.appService.getData();
-  }
+  constructor() {}
 }
--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SharedApiCoreModule } from '@realworld/shared/api/core';
 
 @Module({
   imports: [SharedApiCoreModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
