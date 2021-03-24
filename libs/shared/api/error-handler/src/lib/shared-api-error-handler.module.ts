08a0311fe8138674aaced476bdeab35e9056dc2b
Nhan Nguyen <nhanpublic@gmail.com>
2021-03-25 02:27:36 +0700

    Update error handler

--- libs/shared/api/error-handler/src/lib/shared-api-error-handler.module.ts
+++ libs/shared/api/error-handler/src/lib/shared-api-error-handler.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 @Module({
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    }
+  ],
   exports: [],
 })
 export class SharedApiErrorHandlerModule {}
