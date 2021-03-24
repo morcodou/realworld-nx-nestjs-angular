edfe39f291d761b22d9d712223dcf2327c659e19
Nhan Nguyen <nhanpublic@gmail.com>
2021-03-25 02:30:26 +0700

    update api validations

--- libs/shared/api/validations/src/lib/shared-api-validations.module.ts
+++ libs/shared/api/validations/src/lib/shared-api-validations.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module, ValidationError, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useFactory: () => {
+        return new ValidationPipe({
+          exceptionFactory: (validationErrors: ValidationError[]) => {
+            throw new BadRequestException(validationErrors.toString());
+          },
+        })
+      },
+    },
+  ],
   exports: [],
 })
 export class SharedApiValidationsModule {}
