ccce49cff330bf0fd3e875b2f8e788310787391c
Nhan Nguyen <nhanpublic@gmail.com>
2021-03-25 03:25:24 +0700

    update api config and environment

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
-export const environment = {
+import { IApiConfig } from '@realworld/shared/api/config';
+const packageJson = require('../../../../package.json')
+
+export const environment: IApiConfig = {
   production: true,
+  applicationName: 'Sen Viet API',
+  host: 'http://localhost',
+  port: 3333,
+  version: packageJson.version,
+  debug: true,
+  jwtSecret: 'jwtSecret',
+  jwtExpiresIn: '1y'
 };
--- libs/shared/api/config/src/lib/i-api-config.ts
+++ libs/shared/api/config/src/lib/i-api-config.ts
@@ -1,27 +1,3 @@
-export abstract class IFirebaseConfig { 
-    "type": string
-    "project_id": string
-    "private_key_id": string
-    "private_key": string
-    "client_email": string
-    "client_id": string
-    "auth_uri": string
-    "token_uri": string
-    "auth_provider_x509_cert_url": string
-    "client_x509_cert_url": string
-}
-
-export abstract class IMailerConfig {
-    host: string
-    sender: string
-    port: number
-    secure: boolean // true for 465, false for other ports
-    auth: {
-        user: string
-        pass: string
-    }
-}
-
 export abstract class IApiConfig {
     applicationName?: string
     host?: string
@@ -32,7 +8,4 @@ export abstract class IApiConfig {
     production?: boolean
     debug?: boolean
     webURL?: string
-    portalURL?: string
-    firebase?: IFirebaseConfig
-    mailer?: IMailerConfig
 }
