8f8090388a7f72c57c55c7a4a5ba57bcb2257724
Nhan Nguyen <nhanpublic@gmail.com>
2021-03-28 19:29:45 +0700

    article apis

--- libs/article/api/handlers/src/lib/article-api-handlers.controller.ts
+++ libs/article/api/handlers/src/lib/article-api-handlers.controller.ts
@@ -1,11 +1,12 @@
-import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query, Req } from '@nestjs/common';
-import { IArticle, INewArticle, IUpdateArticle } from '@realworld/article/api-interfaces';
-import { Article, ArticleService, Favorite, FavoriteService } from '@realworld/article/api/shared';
-import { CREATED_MSG, DELETED_MSG, MISSING_REQUIRED_FIELDS_MSG, NOT_FOUND_MSG, UPDATED_MSG } from '@realworld/shared/api/constants';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query, Req } from '@nestjs/common';
+import { IArticle, IComment, INewArticle, INewComment, IUpdateArticle } from '@realworld/article/api-interfaces';
+import { Article, ArticleService, Favorite, FavoriteService, TagService, Comment, CommentService } from '@realworld/article/api/shared';
+import { CREATED_MSG, DELETED_MSG, NOT_FOUND_MSG, UPDATED_MSG } from '@realworld/shared/api/constants';
 import { mapQueriesToFindManyOptions } from '@realworld/shared/api/foundation';
 import { ActionSuccessResponse, DetailSuccessResponse, ListSuccessResponse } from '@realworld/shared/client-server';
 import { StringUtil } from '@realworld/shared/string-util';
 import { Follow, FollowService, SkipAuth, UserService } from '@realworld/user/api/shared';
+import { In } from 'typeorm';
 
 @Controller()
 export class ArticleApiHandlersController {
@@ -13,8 +14,12 @@ export class ArticleApiHandlersController {
         private articleService: ArticleService,
         private userService: UserService,
         private favoriteService: FavoriteService,
-        private followService: FollowService
+        private followService: FollowService,
+        private tagService: TagService,
+        private commentService: CommentService,
     ) { }
+
+    // Article apis
 
     @Post('articles')
     async create(@Req() req, @Body() data: Partial<INewArticle>) {
@@ -23,11 +28,15 @@ export class ArticleApiHandlersController {
             authorId: req?.user?.sub,
             slug: StringUtil.asciiSlug(data.title) + '-' + new Date().getTime()
         }
-
         await this.articleService.insert(article)
+
+        if (data.tagList) {
+            this.updateTags(data.tagList)
+        }
+
         return new ActionSuccessResponse<IArticle>({
             message: CREATED_MSG,
-            data: await this.mapToResponse(article?.authorId, article as Article)
+            data: await this.mapToResponseArticle(article?.authorId, article as Article)
         })
     }
 
@@ -43,16 +52,19 @@ export class ArticleApiHandlersController {
         article = await this.articleService.findOne({ slug: slug })
         return new ActionSuccessResponse<IArticle>({
             message: UPDATED_MSG,
-            data: await this.mapToResponse(req?.user?.sub, article as Article)
+            data: await this.mapToResponseArticle(req?.user?.sub, article as Article)
         })
     }
 
     @Delete('articles/:slug')
     async delete(@Param('slug') slug: string) {
-        if (!slug) {
-            throw new BadRequestException(MISSING_REQUIRED_FIELDS_MSG)
+        const article = await this.articleService.findOne({slug: slug})
+        if (!article) {
+            throw new NotFoundException(NOT_FOUND_MSG)
         }
+
         await this.articleService.softDelete({ slug: slug })
+
         return new ActionSuccessResponse({
             message: DELETED_MSG,
             data: null
@@ -68,7 +80,7 @@ export class ArticleApiHandlersController {
         }
 
         return new DetailSuccessResponse<IArticle>({
-            detailData: await this.mapToResponse(req?.user?.sub, article)
+            detailData: await this.mapToResponseArticle(req?.user?.sub, article)
         })
     }
 
@@ -79,20 +91,116 @@ export class ArticleApiHandlersController {
         let res = await this.articleService.findAll(options)
 
         return new ListSuccessResponse<IArticle>({
-            listData: await Promise.all(res.map(a => this.mapToResponse(req?.user?.sub, a))),
+            listData: await Promise.all(res.map(a => this.mapToResponseArticle(req?.user?.sub, a))),
             total: await this.articleService.count(options)
         })
     }
     
     @Get('articles/feed')
     async findAllFeed(@Req() req, @Query() query) {
-        const follow = this.followService.findAll(mapQueriesToFindManyOptions<Follow>({}))
-        const options = mapQueriesToFindManyOptions<Article>(query, 'title', 'slug', 'shortDescription', 'body')
+        const follows = await this.followService.findAll(mapQueriesToFindManyOptions<Follow>({followerId: req?.user?.sub}))
+        const followedIds = follows.map(f => f.followedId)
+
+        const options = mapQueriesToFindManyOptions<Article>({
+            ...query,
+            authorId: In(followedIds)
+        })
         let res = await this.articleService.findAll(options)
 
         return new ListSuccessResponse<IArticle>({
-            listData: await Promise.all(res.map(a => this.mapToResponse(req?.user?.sub, a))),
+            listData: await Promise.all(res.map(a => this.mapToResponseArticle(req?.user?.sub, a))),
             total: await this.articleService.count(options)
+        })
+    }
+
+    // Favorite apis
+
+    @Post('articles/:slug/favorite')
+    async favoriteAnArticle(@Req() req, @Param('slug') slug) {
+        const isFavorited = !!this.favoriteService.findOne({userId: req?.user?.sub, articleSlug: slug})
+        if (!isFavorited) {
+            await this.favoriteService.insert({userId: req?.user?.sub, articleSlug: slug})
+        }
+        const article = await this.articleService.findOne({ slug: slug })
+        return new ActionSuccessResponse<IArticle>({
+            message: '',
+            data: await this.mapToResponseArticle(req?.user?.sub, article as Article)
+        })
+    }
+    
+    @Delete('articles/:slug/favorite')
+    async unfavoriteAnArticle(@Req() req, @Param('slug') slug) {
+        const isFavorited = !!this.favoriteService.findOne({userId: req?.user?.sub, articleSlug: slug})
+        if (isFavorited) {
+            await this.favoriteService.softDelete({userId: req?.user?.sub, articleSlug: slug})
+        }
+        const article = await this.articleService.findOne({ slug: slug })
+        return new ActionSuccessResponse<IArticle>({
+            message: '',
+            data: await this.mapToResponseArticle(req?.user?.sub, article as Article)
+        })
+    }
+
+    // Comment apis
+
+    @Get('articles/:slug/comments')
+    async findAllComments(@Req() req, @Param('slug') slug: string) {
+        const options = mapQueriesToFindManyOptions<Comment>({articleSlug: slug})
+        let res = await this.commentService.findAll(options)
+
+        return new ListSuccessResponse<IComment>({
+            listData: await Promise.all(res.map(c => this.mapToResponseComment(req?.user?.sub, c))),
+            total: await this.commentService.count(options)
+        })
+    }
+    
+    @Post('articles/:slug/comments')
+    async createAComment(@Req() req, @Param('slug') slug: string, @Body() data: INewComment) {
+        let comment: Partial<Comment> = {
+            ...data,
+            authorId: req?.user?.sub,
+            articleSlug: slug
+        }
+        await this.commentService.insert(comment)
+
+        return new ActionSuccessResponse<IComment>({
+            message: '',
+            data: await this.mapToResponseComment(comment?.authorId, comment as Comment)
+        })
+    }
+    
+    @Delete('articles/:slug/comments/:id')
+    async deleteAComment(@Param('slug') slug: string, @Param('id') id: string) {
+        await this.commentService.softDelete({articleSlug: slug, id: id})
+        return new ActionSuccessResponse({
+            message: DELETED_MSG,
+            data: null
+        })
+    }
+
+    // Tag apis
+
+    @SkipAuth()
+    @Get('tags')
+    async findAllTags(@Req() req, @Query() query) {
+        let tags = await this.tagService.findAll()
+
+        return new ListSuccessResponse<string>({
+            listData: tags.map(t => t.name),
+            total: await this.tagService.count()
+        })
+    }
+
+    // Helper functions
+
+    private updateTags(tagList: string[]) {
+        tagList.forEach(async t => {
+            t = t.trim()
+            const tagExists = !!(await this.tagService.findOne({name: t}))
+            if (!tagExists) {
+                this.tagService.insert({name: t})
+            }
+            
         })
     }
 
@@ -105,12 +213,19 @@ export class ArticleApiHandlersController {
         return await this.favoriteService.count(options)
     }
 
-    mapToResponse = async (requestUserId: string, article: Article): Promise<IArticle> => {
+    mapToResponseArticle = async (requestUserId: string, article: Article): Promise<IArticle> => {
         return {
             ...article,
             favorited: await this.didUserFavoriteThisArticle(requestUserId, article?.slug),
             favoritesCount: await this.getArticleFavoritesCount(article?.slug),
             author: await this.userService.getProfile(requestUserId, article?.authorId)
+        }
+    }
+
+    mapToResponseComment = async (requestUserId: string, comment: Comment): Promise<IComment> => {
+        return {
+            ...comment,
+            author: await this.userService.getProfile(requestUserId, comment?.authorId)
         }
     }
 }
