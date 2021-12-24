c90464f2b4fec73c54e4bf29f36364cec559c1c0
Mor Codou GUEYE <morcodou@gmail.com>
2021-12-24 11:18:22 -0500

    Add user login test

--- apps/conduit-e2e/src/integration/app.spec.ts
+++ apps/conduit-e2e/src/integration/app.spec.ts
@@ -1,17 +1,17 @@
 import { getGreeting } from '../support/app.po';
 
 describe('conduit', () => {
-  // beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/'));
 
-  // it('should display welcome message', () => {
-  //   // cy.visit('http://localhost:4200');
+  it('should display conduit message', () => {
+    // cy.visit('http://localhost:4200');
 
-  //   // Custom command example, see `../support/commands.ts` file
-  //   cy.login('user2@email.com', 'qwerty1');
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('user2@email.com', 'qwerty1');
 
-  //   // Function helper example, see `../support/app.po.ts` file
-  //   getGreeting().contains('conduit');
-  // });
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('conduit');
+  });
 
   it('test url works', () => {
     cy.visit('http://localhost:4200');
