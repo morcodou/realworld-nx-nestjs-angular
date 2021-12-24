fd215a63350371b7beff9c3293d6299faf9e92f8
Mor Codou GUEYE <morcodou@gmail.com>
2021-12-24 00:14:04 -0500

    Setup cypress and write first cypress test

--- apps/conduit-e2e/src/integration/app.spec.ts
+++ apps/conduit-e2e/src/integration/app.spec.ts
@@ -1,13 +1,23 @@
 import { getGreeting } from '../support/app.po';
 
 describe('conduit', () => {
-  beforeEach(() => cy.visit('/'));
+  // beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  // it('should display welcome message', () => {
+  //   // cy.visit('http://localhost:4200');
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to conduit!');
+  //   // Custom command example, see `../support/commands.ts` file
+  //   cy.login('user2@email.com', 'qwerty1');
+
+  //   // Function helper example, see `../support/app.po.ts` file
+  //   getGreeting().contains('conduit');
+  // });
+
+  it('test url works', () => {
+    cy.visit('http://localhost:4200');
+  });
+
+  it('test sign up exists', () => {
+    cy.contains('a.nav-link', 'Sign up').click();
   });
 });
